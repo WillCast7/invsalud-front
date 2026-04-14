25d71123547f4e7245334cb90aba8b4576962140
William <williamisrael210@gmail.com>
2026-04-14 16:56:45 -0500

    bakcend connection fixed

--- src/environment/aurea.ts
+++ src/environment/aurea.ts
@@ -3,8 +3,9 @@ export const config = {
   apiVersion: 'v1',
   urlBackend: 'http://76.13.30.238:9091',
   urlMedia: 'http://76.13.30.238:9091',
-  //urlMedia: 'http://localhost:9090',
-  //urlBackend: 'http://localhost:9090',
+  //urlMedia: 'http://localhost:9091',
+  //urlBackend: 'http://localhost:9091',
+
 
   // masks
   maskPhone: "(000) 000 00 00",
