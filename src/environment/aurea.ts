4a70261cb24edb7e1305a08ff374e29bf33b3e55
William <williamisrael210@gmail.com>
2026-04-14 10:40:50 -0500

    editing for test

--- src/environment/aurea.ts
+++ src/environment/aurea.ts
@@ -1,10 +1,10 @@
 // api configuration Here!
 export const config = {
   apiVersion: 'v1',
-  //urlBackend: 'http://76.13.30.238:9090',
-  //urlMedia: 'http://76.13.30.238:9090',
-  urlMedia: 'http://localhost:9090',
-  urlBackend: 'http://localhost:9090',
+  urlBackend: 'http://76.13.30.238:9091',
+  urlMedia: 'http://76.13.30.238:9091',
+  //urlMedia: 'http://localhost:9090',
+  //urlBackend: 'http://localhost:9090',
 
   // masks
   maskPhone: "(000) 000 00 00",
